8a44f45fd06888105d4cc3bed3991e367c3e0e2b
Gustavo Martins <gustao_meiramartins@outlook.com>
2026-06-14 19:27:17 -0300

    chore: prepara aplicação para deploy

--- aula5/app.py
+++ aula5/app.py
@@ -1,10 +1,20 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "API rodando"
+    return "API rodando muito bem!"
+
+@app.route("/status")
+def status():
+    return {"status": "ok"}
+
+@app.route("/soma/<int:a>/<int:b>")
+def soma(a, b):
+    return {"resultado": a + b}
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
